610beca65155ed8c98376a12758d40d434d854b2
rchronic <radhityachronicle@gmail.com>
2017-10-21 10:04:29 +0700

    first commit

---
+++ apap_tutorial_6/src/main/java/com/example/tutorial6/ApapTutorial6Application.java
@@ -0,0 +1,12 @@
+package com.example.tutorial6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutorial6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApapTutorial6Application.class, args);
+	}
+}
